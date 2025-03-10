5bf28ec11ccb1bb1f6ad8ad1a4cdca62daf8210a
Dennis Turco <dennisturco@gmail.com>
2025-03-10 10:32:04 +0100

    projects page fix

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -2,8 +2,20 @@ import "/styles/global.scss"
 import { Navbar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
 import Hero from "../components/Hero2";
-import MapsPosition from "../components/MapsPosition";
 import Projects from "../components/Projects";
+import { motion } from "framer-motion";
+import sanityClient from "@sanity/client";
+
+const fadeIn = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+  };
+  
+  const client = sanityClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    useCdn: true,
+  });
 
 export default function HomePage() {
     return (
@@ -13,7 +25,7 @@ export default function HomePage() {
             <div><Navbar /></div>
 
             {/* Hero Section */}
-            <section><Hero /></section>
+            <section><Hero imagePath={'/images/catsBanner.jpg'} title={""} /></section>
 
             {/* Projects list */}
             <div><Projects /></div>
